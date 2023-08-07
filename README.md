2a0783317d171788cafb1ec292aec7cf09423bf6
YaqeenAdel <helsadani01124@gmail.com>
2023-08-07 23:39:22 +0300

    last commit

--- README.md
+++
@@ -1,2 +0,0 @@
-Yaqeen Volunteer website 
-<a href="url">[link text](https://yaqeenadel.github.io/about-Yaqeen.github.io/)https://yaqeenadel.github.io/about-Yaqeen.github.io/</a>
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 function OnclickOpenMenu() {
-  document.getElementById("header-nav").style.display = "block";
-  document.getElementById("close-menu").style.display = "block";
-  document.getElementById("open-menu").style.display = "none";
+  document.getElementById("header-nav").classList.remove("hidden")
+  document.getElementById("close-menu").classList.remove("hidden")
+  document.getElementById("open-menu").classList.add("hidden")
 }
 
 function OnclickCloseMenu() {
-  document.getElementById("header-nav").style.display = "none";
-  document.getElementById("close-menu").style.display = "none";
-  document.getElementById("open-menu").style.display = "block";
+  document.getElementById("header-nav").classList.add("hidden")
+  document.getElementById("close-menu").classList.add("hidden")
+  document.getElementById("open-menu").classList.remove("hidden")
 }
 
 // window.onscroll = function () {
@@ -17,19 +17,43 @@ function OnclickCloseMenu() {
 //   window.scrollTo({ top: 0, behavior: "smooth" });
 // }
 
+function SetCookies(value){
+  var expiryDate = new Date();
+  expiryDate.setMonth(expiryDate.getMonth() + 1);
+  document.cookie = `dark-mode=${value}; expires=${expiryDate}; path=/`
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+} 
+
 (function () {
-  console.log(localStorage.getItem("dark-mode"));
-  if (localStorage.getItem("dark-mode") === "On") {
+  console.log(getCookie('dark-mode'));
+  if (getCookie('dark-mode') == "On") {
     document.documentElement.classList.add("dark");
   }
+  else if (getCookie('dark-mode') == "Off") {
+    document.documentElement.classList.remove("dark");
+  }else if (window.matchMedia) {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      SetCookies('On')
+      document.documentElement.classList.add("dark")
+    }else{
+      SetCookies('Off')
+      document.documentElement.classList.remove("dark")
+    }
+  }
+
 })();
 
 function darkmode() {
-  localStorage.setItem("dark-mode", "On");
+  SetCookies('On')
   document.documentElement.classList.add("dark");
 }
 
 function lightmode() {
-  localStorage.setItem("dark-mode", "Off");
+  SetCookies('Off')
   document.documentElement.classList.remove("dark");
 }
